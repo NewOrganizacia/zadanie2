519d0e7cbca7f5687a878901b87a739293f02c88
naruta6 <61300723+naruta6@users.noreply.github.com>
2020-05-22 17:04:44 +0700

    add php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+require_once 'application/bootstrap.php';
+
+//$GLOBALS['site'] = 'localhost';
+//$GLOBALS['login'] = 'root';
+//$GLOBALS["password"] = '';
+//$GLOBALS['database'] = 'portfolio';
+//
+//
+//mysqli_set_charset($data = new mysqli(
+//    $GLOBALS['site'],
+//    $GLOBALS['login'],
+//    $GLOBALS["password"],
+//    $GLOBALS['database']),
+//    'utf8')or die('Error');
+//
+//echo '<tr><td>'.$data['Name'].'</td><td>'.$data['Data'].'</td><td>'.$data['mail'].'</td><td>'.$data['info'].'</td></tr>';
+
+?>
+
+
